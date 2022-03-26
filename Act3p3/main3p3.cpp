8bfe0aef2ed4c2081e9b36de788b955765a174f0
Jeshua N <pigihunter@gmail.com>
2022-03-25 19:07:45 -0600

    Creation of compiler file is done

--- Act3p3/main3p3.cpp
+++ Act3p3/main3p3.cpp
@@ -16,20 +16,20 @@ Date of creation and last modification:
 std::vector<std::string> regExpressions;
 std::vector<std::string> tokensTypeName;
 /**
- * @brief Creates a Flex file for our re-gex motor.
+ * @brief Creates a Flex file for our reg-ex motor.
  * 
- * @param path Where the .txt file is located (with the re-gex).
+ * @param path Where the .txt file is located (with the reg-ex).
  */
 void createFlexFile(std::string path){
     int middlePointLine = 0;
-    std::string re_gex;
+    std::string reg_ex;
     std::string tokenType;
     std::string line;
     // Create and open file exprMoto.l
     std::ofstream myFile("exprMoto1.l");
     myFile << "%{\n\n%}\n\n%%\n";
     // Insert our regular expressions.
-    std::ifstream txtFile(path.c_str()); // Opens our re-gex file.
+    std::ifstream txtFile(path.c_str()); // Opens our reg-ex file.
     while (getline(txtFile, line)){
         // Traverse line and finds the regular expression and its type.
         for (int i = 0; i < line.length(); i++){
@@ -39,10 +39,10 @@ void createFlexFile(std::string path){
             }
         }
         if (middlePointLine != 0){
-            re_gex = line.substr(0, middlePointLine - 1);
+            reg_ex = line.substr(0, middlePointLine - 1);
             tokenType = line.substr(middlePointLine + 2, line.length()-1);
             //std::cout << re_gex << " {fprintf(yyout, \"" << tokenType << " \");fprintf(yyout, yytext);fprintf(yyout, \"\\n\");}" << std::endl;
-            myFile << re_gex << " {fprintf(yyout, \"" << tokenType << " \");fprintf(yyout, yytext);fprintf(yyout, \"\\n\");}" << "\n";
+            myFile << reg_ex << " {fprintf(yyout, \"" << tokenType << " \");fprintf(yyout, yytext);fprintf(yyout, \"\\n\");}" << "\n";
         }
     }
     
@@ -50,8 +50,24 @@ void createFlexFile(std::string path){
     myFile << "%%\nint yywrap(void){return 1;}";
     myFile.close();
 }
+/**
+ * @brief Creates a Cpp file so we can compile our Flex file. 
+ * 
+ */
+void createCompilerCpp(){
+    // Create and open file compiler1.cpp
+    std::ofstream myFile("compiler1.cpp");
+    // Insert all neccesary info.
+    myFile << "#include <iostream>\n#include <stdlib.h>\n#include <stdio.h>\n\n";
+    myFile << "extern int yylex();\nextern int yylineno;\nextern char* yytext;\n\n";
+    myFile << "int main(){\n\textern FILE *yyin, *yyout;\n\t";
+    myFile << "yyin = fopen(\"inputText.txt\", \"r\");\n\t";
+    myFile << "yyout = fopen(\"outputTokens.txt\", \"w\");\n\t";
+    myFile << "yylex();\n\treturn 0;\n}";
+}
 
 int main(){
     createFlexFile("inputRegex.txt");
+    createCompilerCpp();
     return 0;
 }
