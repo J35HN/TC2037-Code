012ecddf0426837d50ba750427bef5618d4b6a87
Jeshua N <pigihunter@gmail.com>
2022-03-26 09:22:03 -0600

    Definitive commit for temp folder

---
+++ tempF/compiler.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+extern int yylex();
+extern int yylineno;
+extern char* yytext;
+
+int main(){
+    extern FILE *yyin, *yyout;
+    yyin = fopen("inputText.txt", "r");
+    yyout = fopen("outputTokens.txt", "w");
+    yylex();
+    return 0;
+}
